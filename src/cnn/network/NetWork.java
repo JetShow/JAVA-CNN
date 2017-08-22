18e08ce6552fc31f588de1e4e990f5d19e4dc347
ChenShaojie <chenshaojie@csair.com>
2017-08-22 23:22:17 +0800

    network

---
+++ src/cnn/network/NetWork.java
@@ -0,0 +1,29 @@
+package cnn.network;
+
+import java.util.List;
+
+public class NetWork {
+	List<Object> layers;
+	
+	public boolean train()
+	{
+		return false;
+	}
+	
+	public boolean trainOnce()
+	{
+		return false;
+	}
+	
+	public boolean trainOnceBatch()
+	{
+		return false;
+	}
+	
+	public float predict()
+	{
+		return 0;
+	}
+	
+	public 
+}
